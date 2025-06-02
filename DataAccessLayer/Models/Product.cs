1f3fa7c180fb7bd4e60cdc926634ff521e173aaf
TyrannousGraph9 <2405594cremers@zuyd.nl>
2025-06-02 15:13:34 +0200

    added stock to product

--- DataAccessLayer/Models/Product.cs
+++ DataAccessLayer/Models/Product.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace DataAccessLayer.Models
 {
     public class Product
-    {        
+    {
         public int Id { get; set; }
 
         public string Name { get; set; }
@@ -20,5 +20,6 @@ namespace DataAccessLayer.Models
         public ICollection<Order> Orders { get; } = new List<Order>();
 
         public ICollection<Part> Parts { get; } = new List<Part>();
+        public int Stock { get; set; }
     }
 }
