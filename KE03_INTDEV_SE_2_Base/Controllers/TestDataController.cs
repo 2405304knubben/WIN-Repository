29bc9b5bfaa4db0959945dbeb2b6d052afe928f0
SemDuvan <2403737duvan@zuyd.nl>
2025-06-04 07:27:23 +0200

    zorgt voor een random orderom de grafiek te testren

--- KE03_INTDEV_SE_2_Base/Controllers/TestDataController.cs
+++ KE03_INTDEV_SE_2_Base/Controllers/TestDataController.cs
@@ -15,22 +15,26 @@ namespace KE03_INTDEV_SE_2_Base.Controllers
         }
 
         public IActionResult AddTestData()
-        {
-            // Voeg eerst een order toe voor vandaag
-            var customer = _context.Customers.First();
-            var product = _context.Products.First();
+        {            var random = new Random();
+            
+            // Haal alle customers en products op
+            var customers = _context.Customers.ToList();
+            var products = _context.Products.ToList();
+            var aantal = random.Next(1, 6); // Random aantal tussen 1 en 5 (Next is exclusive op de upper bound)
+            
+            // Kies random een customer en product
+            var customer = customers[random.Next(customers.Count)];
+            var product = products[random.Next(products.Count)];
             
             var order = new Order 
             { 
                 Customer = customer,
                 OrderDate = DateTime.Now
-            };
-
-            var orderProduct = new OrderProduct
+            };            var orderProduct = new OrderProduct
             {
                 Order = order,
                 Product = product,
-                Aantal = 2
+                Aantal = aantal
             };
 
             order.OrderProducts.Add(orderProduct);
